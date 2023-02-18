d1bb84d6bfe63c569de1fbda8e2998238653da0c
shambhavee <shambhaveesrivastava@gmail.com>
2023-02-18 22:36:28 +0530

    second commit

--- src/main/java/mypkgg2/displayit.java
+++ src/main/java/mypkgg2/displayit.java
@@ -1,10 +1,21 @@
 package mypkgg2;
 
 public class displayit {
+	public void myshow() {
+		System.out.println("hello devops! Welcome to jenkins");
+	}
+	public void myshow2() {
+		System.out.println("hello shambhavee!");
+	}
+	public void diso() {
+		System.out.println("another build");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		displayit obj = new displayit();
+		obj.myshow();
+		obj.myshow2();
 	}
 
 }
